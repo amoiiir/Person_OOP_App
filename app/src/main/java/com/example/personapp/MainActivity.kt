a9b1a33233eb39baaee7d333b2ec7f3103d1b59e
Amir Hamzah <mahamzah535@gmail.com>
2024-03-07 10:20:14 +0800

    Now can print user input on the body

--- app/src/main/java/com/example/personapp/MainActivity.kt
+++ app/src/main/java/com/example/personapp/MainActivity.kt
@@ -5,16 +5,32 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userName: EditText
+    private lateinit var btnSubmit: Button
+    private lateinit var displayName: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val person = Person()
+        person.userName = findViewById(R.id.etName)
+        person.btnSubmit = findViewById(R.id.btnSubmit)
+        person.displayName = findViewById(R.id.displayName)
+
+        person.btnSubmit.setOnClickListener{
+            val message = person.userName.text.toString()
+            person.displayName.text = "Hello, $message !"
+//            Toast.makeText(this,"Your name is $message",Toast.LENGTH_LONG).show()
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/com/example/personapp/Person.kt
+++ app/src/main/java/com/example/personapp/Person.kt
@@ -1,11 +1,14 @@
 package com.example.personapp
 
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Person {
 
-    fun details(){
-        println("Test")
-    }
+    lateinit var userName: EditText
+    lateinit var btnSubmit: Button
+    lateinit var displayName: TextView
 
 }
